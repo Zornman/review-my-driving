193a76bf4ac6e624fe27caa99360954a9ffba984
Alec Zorn <zornman45@gmail.com>
2025-03-02 12:31:39 -0700

    Removed unused method

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,17 +1,4 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
-import { readFileSync } from 'fs';
-import { join } from 'path';
-
-export function getPrerenderParams(): Record<string, string[]> {
-  try {
-    const filePath = join(process.cwd(), 'src/assets/products.txt');
-    const productIds = readFileSync(filePath, 'utf-8').split('\n').map(id => id.trim()).filter(Boolean);
-    return { id: productIds };
-  } catch (error) {
-    console.error('Error reading products.txt:', error);
-    return { id: [] };
-  }
-}
 
 export const serverRoutes: ServerRoute[] = [
   {
