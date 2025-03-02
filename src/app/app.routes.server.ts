e5bf60288358b750d93acc6421955c8336a402c5
Alec Zorn <zornman45@gmail.com>
2025-03-02 12:06:32 -0700

    Updates to app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,29 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+export function getPrerenderParams(): Record<string, string[]> {
+  try {
+    const filePath = join(process.cwd(), 'src/assets/products.txt');
+    const productIds = readFileSync(filePath, 'utf-8').split('\n').map(id => id.trim()).filter(Boolean);
+    return { id: productIds };
+  } catch (error) {
+    console.error('Error reading products.txt:', error);
+    return { id: [] };
+  }
+}
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'product/:id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'orderConfirmation/:id',
+    renderMode: RenderMode.Client // Disable prerendering for orderConfirmation
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Prerender // Keep prerendering for all other routes
   }
 ];
