ca1a9df598088eefbb04974aa32ec02ad0df630f
Alec Zorn <zornman45@gmail.com>
2025-03-03 11:49:33 -0700

    Updates to environment variables

--- netlify/functions/getUserByUID.js
+++ netlify/functions/getUserByUID.js
@@ -1,7 +1,9 @@
 const admin = require("firebase-admin");
 
 // Parse JSON from Netlify environment variable
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+const serviceAccount = JSON.parse({ "type": "service_account", "project_id": "review-my-driving", "private_key_id": process.env.FIREBASE_SERVICE_PRIVATE_KEY_ID, "private_key": "-----BEGIN PRIVATE KEY-----\\n" + process.env.FIREBASE_SERVICE_ACCOUNT_KEY + "\\n-----END PRIVATE KEY-----\\n", "client_email": "firebase-adminsdk-9ltlf@review-my-driving.iam.gserviceaccount.com", "client_id": "117338358802601525313", "auth_uri": "https://accounts.google.com/o/oauth2/auth", "token_uri": "https://oauth2.googleapis.com/token", "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs", "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-9ltlf%40review-my-driving.iam.gserviceaccount.com", "universe_domain": "googleapis.com" });
+
+
 
 // Fix private key formatting (convert `\\n` to real `\n`)
 serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ function fetchConfig(): Promise<any> {
   return fetch('/.netlify/functions/getEnvironmentVariables')
     .then(response => response.json())
     .then(config => {
-      return config.FIREBASE_SERVICE_ACCOUNT;
+      return { "type": "service_account", "project_id": "review-my-driving", "private_key_id": config.FIREBASE_SERVICE_PRIVATE_KEY_ID, "private_key": "-----BEGIN PRIVATE KEY-----\\n" + config.FIREBASE_SERVICE_ACCOUNT_KEY + "\\n-----END PRIVATE KEY-----\\n", "client_email": "firebase-adminsdk-9ltlf@review-my-driving.iam.gserviceaccount.com", "client_id": "117338358802601525313", "auth_uri": "https://accounts.google.com/o/oauth2/auth", "token_uri": "https://oauth2.googleapis.com/token", "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs", "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-9ltlf%40review-my-driving.iam.gserviceaccount.com", "universe_domain": "googleapis.com" };
     })
     .catch(error => {
       console.error('Error loading Netlify config:', error);
