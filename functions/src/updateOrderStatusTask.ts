cffb4413756d16f338c03e513ad4e59c867ca822
Alec Zorn <zornman45@gmail.com>
2025-03-11 15:33:08 -0700

    Updates to functions

--- functions/src/updateOrderStatusTask.ts
+++ functions/src/updateOrderStatusTask.ts
@@ -83,24 +83,74 @@ export async function updateOrderStatusTask(secrets?: {
   }
 }
 
-// ✅ Email Sending Function
-async function sendEmail(user: string, pass: string, printifyOrder: any) {
+async function sendEmail(emailUser: string, emailPass: string, printifyOrder: any) {
   console.log("Sending email to " + printifyOrder.address_to.email);
 
+  // Set up Nodemailer transporter
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    service: "gmail", // Use Gmail or another email service
     auth: {
-      user: (user) ? user : functions.params.defineSecret("EMAIL_USER").value(),
-      pass: (pass) ? pass : functions.params.defineSecret("EMAIL_PASS").value(),
+      user: emailUser, // Your email address
+      pass: emailPass, // Your email password (use an app password for Gmail)
     },
   });
 
+  // Email options
   const mailOptions = {
     from: "review.mydriving1@gmail.com",
     to: printifyOrder.address_to.email,
-    subject: `Order Update: ${printifyOrder.id}`,
-    text: "Your order status has been updated.",
+    subject: getEmailSubject(printifyOrder),
+    html: "<html><head><style type=\"text/css\">@import url(\"https://fonts.googleapis.com/css?family=DynaPuff\");body {font-family: \"DynaPuff\", Arial, serif;}</style><style>table {border-collapse: collapse;width: 100%;}td, th {border: 1px solid #dddddd;text-align: left;padding: 8px;}tr:nth-child(even) {background-color: #dddddd;</style></head><body>" + getEmailContents(printifyOrder) + "</body></html>",
   };
 
+  // Simulate asynchronous email sending
   return await transporter.sendMail(mailOptions);
+}
+      
+function getEmailSubject(order: any): string {
+  if (order.status === "in-production") {
+    return "Order In Production #" + order.id;
+  } else if (order.status === "fulfilled") {
+    return "Your Order has Shipped! #" + order.id;
+  } else if (order.status === "canceled") {
+    return "Order Canceled #" + order.id;
+  } else {
+    return "Order Status Updated #" + order.id;
+  }
+}
+      
+function getEmailContents(order: any): string {
+  if (order.status === "in-production") {
+    let html = "<h1 style=\"text-align: center;\">Review My Driving</h1><h2 style=\"color: #4B0082;\">Your order is being custom crafted!</h2><p><strong>Order Number:  </strong> " + order.id + "</p><p><strong>Order Total:  </strong>" + calculateOrderTotal(order) + "</p>";
+    html += "<h3><strong>Order Details:</strong></h3><table><tr><th>Title</th><th>Qty</th><th>Price</th></tr>";
+
+    for (const line_item of order.line_items) {
+      const md = line_item.metadata;
+      html += "<tr><td>" + md.title.split("-")[0] + "</td><td style=\"text-align: center;\">${line_item.quantity}</td><td>" + ((md.price * line_item.quantity) / 100) + "</td></tr>";
+    }
+
+    html += "</table>";
+
+    html += "<hr style=\"border: 1px solid #ccc;\" /><p style=\"font-size: 0.9em; color: #555;\">This email was generated automatically by <a href=\"www.reviewmydriving.co\">reviewmydriving.co</a></p>";
+
+    return html;
+  } else if (order.status === "fulfilled") {
+    return "<h1 style=\"text-align: center;\">Review My Driving</h1><h2 style=\"color: #4B0082;\">Your order has shipped!</h2><p><strong>Order Number:  </strong> ${order.id}</p><p><strong>Date Shipped:  </strong> ${order.fulfilled_at}</p><p><strong>Tracking Info: </strong></p><p>Carrier - ${order.shipments[0].carrier}</p><p>Tracking #<a href=\"${order.shipments[0].url}\">" + order.shipments[0].number + "</a></p><hr style=\"border: 1px solid #ccc;\" /><p style=\"font-size: 0.9em; color: #555;\">This email was generated automatically by <a href=\"www.reviewmydriving.co\">reviewmydriving.co</a></p>";
+  } else if (order.status === "canceled") {
+    return "<h1 style=\"text-align: center;\">Review My Driving</h1><h2 style=\"color: #4B0082;\">Your order has been canceled.</h2><p><strong>Order Number:  </strong>" + order.id + "</p><p>This order has been canceled. No further action is required on your end.</p><hr style=\"border: 1px solid #ccc;\" /><p style=\"font-size: 0.9em; color: #555;\">This email was generated automatically by <a href=\"www.reviewmydriving.co\">reviewmydriving.co</a></p>";
+  } else {
+    return "<h1 style=\"text-align: center;\">Review My Driving</h1><h2 style=\"color: #4B0082;\">Order Update Email</h2><p><strong>Order Number:  </strong>" + order.id + "</p><p>This order has been updated. No further action is required on your end.</p><hr style=\"border: 1px solid #ccc;\" /><p style=\"font-size: 0.9em; color: #555;\">This email was generated automatically by <a href=\"www.reviewmydriving.co\">reviewmydriving.co</a></p>";
+  }
+}
+
+function calculateOrderTotal(order: any): number {
+  let orderTotal = 0;
+
+  for (const line_item of order.line_items) {
+    const price = line_item.metadata.price;
+    const line_total = price * line_item.quantity;
+    orderTotal += line_total;
+  }
+
+  return (orderTotal / 100);
 }
